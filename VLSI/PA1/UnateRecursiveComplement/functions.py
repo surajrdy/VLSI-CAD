bd14ae969a371df1d91a4eddbf897e11231d90ef
surajrdy <surajmvr14@gmail.com>
2024-06-20 16:43:55 -0400

    v2

--- VLSI/PA1/UnateRecursiveComplement/functions.py
+++ VLSI/PA1/UnateRecursiveComplement/functions.py
@@ -2,54 +2,54 @@ from pcn import PCN_create
 from io import StringIO
 import sys
 
-
-
-filename = sys.argv[1]
-with open(filename, 'r') as file:
-    file_content = file.read()
-
-pcn_list, nvars, ncubes = PCN_create(file_content)
+answer = None
 
 
 #Termination Cases
-def DeMorgans(pcn, nvars, ncubes):
+def DeMorgans(func, nvars):
     #Check for new cube list size
     complement = [[['11'] * nvars] * non_dc]
     non_dc = nvars
     counter = 0
-    for x in range(0, nvars-1):
-        if pcn[0][x] == '11':
+    for x in range(nvars):
+        if func[0][x] == '11':
             non_dc - 1
             counter+=1
     #All don't care Cube
     if counter == nvars:
         complement = [['']]
     c = 0
-    for x in range(0, nvars-1):
-        if pcn[0][x] == "01":
+    for x in range(nvars):
+        if func[0][x] == "01":
             complement[c][x] == "10"
             c += 1
-        elif pcn[0][x] == "10":
+        elif func[0][x] == "10":
             complement[c][x] == "01"
             c += 1
         else: 
             c+1
     return complement
 
-def Complement(pcn, nvars, ncubes):
-    if len(pcn) == 0:
+def Final(func, nvars, ncubes):
+    if len(func) == 0:
         return [['11'] * nvars]
-    elif len(pcn) == 1:
-        return DeMorgans(pcn, nvars, ncubes)    
+    elif len(func) == 1:
+        return DeMorgans(func, nvars)    
     else:
         counterDC = 0
-        for x in range(0, ncubes-1):
+        for x in range(ncubes):
             for y in range(0, nvars):
-                if pcn[x] == [['11' * nvars]]:
+                if func[x] == [['11' * nvars]]:
                     return [['']]
         #Code bulk of project
-        z = BinateSelection(pcn, nvars, ncubes)
-
+        z = BinateSelection(func, nvars, ncubes)
+        P = CoFactor(func, z, 'true', ncubes)
+        N = CoFactor(func, z, 'neg', ncubes)
+        P_n = Final(P, nvars)
+        N_n = Final(N, nvars)
+        P_c = AND(z, P_n, 'true')
+        N_c = AND(z, N_n, 'comp')
+        answer = OR(P_c, N_c)
         return answer
     return answer
 
@@ -62,23 +62,23 @@ def BinateVariable(function, nvars, ncubes):
     binate_list = [False] *nvars
     binate = False
 
-    for x in range(0, ncubes):
+    for x in range(ncubes):
         for y in range(0, nvars-1):
             if cubes[x][y] == '01':
                 true_count[y] += 1
             elif cubes[x][y] == '10':
                 neg_count[y] += 1
     
-    for y in range(0, nvars-1):
+    for y in range(nvars):
         if true_count[y] > 0 and neg_count > 0:
             binate_list[y] = True
             binate = True
 
     return true_count, neg_count, binate_list, binate
 
-def BinateSelection(pcn, nvars, ncubes):
+def BinateSelection(function, nvars, ncubes):
 
-    true_count, neg_count, binate_list, binate = BinateVariable(pcn, nvars, ncubes)
+    true_count, neg_count, binate_list, binate = BinateVariable(function, nvars, ncubes)
 
     if binate == True:
         final_binate = None
@@ -121,12 +121,12 @@ def BinateSelection(pcn, nvars, ncubes):
     else:
         max_unate = 0
         winner = None
-        for y in range(0, nvars-1):
+        for y in range(nvars):
             if true_count[y] or neg_count[y] > max_unate:
                 max_unate = max(true_count[y], neg_count[y])
                 winner = y
             else:
-                for x in range(0, nvars-1):
+                for x in range(nvars):
                     if true_count[x] or neg_count[x] == max_unate:
                         winner = x
         return winner
@@ -142,16 +142,62 @@ def OR(cofunction1, cofunction2):
 def AND(cofunction, mostbinate, factor):
     for i in range(len(cofunction)):
         if factor == 'comp':
-            cofunction[i][mostbinate] = '10'
+            cofunction[i][mostbinate-1] = '10'
         elif factor =='true':
-            cofunction[i][mostbinate] = '01'
+            cofunction[i][mostbinate-1] = '01'
     return cofunction
 
     
     return
-def CoFactor(function, mostbinate, term):
+def CoFactor(function, mostbinate, term, ncubes):
     cofactor = []
+    terminator = None
+    opposite = None
+    cube_count = 0
+    if term == 'true':
+        terminate = '10'
+        opposite = '01'
+    else:
+        terminate = '01'
+        opposite = '10'
+    for i in range(ncubes):
+        if function[i][mostbinate-1] != terminator:
+            cube_count += 1
+            new_cube = function[i][:]
+            for j in range(len(function[i])):
+                new_cube[cube_count][j] = function[i][j]
+                if j == mostbinate-1:
+                    if function[i][j] == opposite:
+                        new_cube[cube_count][j] = '11'
+            cofactor.append(new_cube)
+    return cofactor
 
-    for i in range(ncubes-1):
-        
-    return
+def format(answer):
+    output = [0]
+    for x in answer:
+        counter = 0
+        for y in answer[x]:
+            if y != '11':
+                counter += 1
+                if y == '01':
+                    output[x][counter] == str(y)
+                elif y =='10':
+                    output[x][counter] == '-' + str(y)
+    return output
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python pcn_reader.py <filename>")
+        return
+
+    filename = sys.argv[1]
+    with open(filename, 'r') as file:
+            file_content = file.read()
+
+    pcn_list, nvars, ncubes = PCN_create(file_content)
+    output = Final(pcn_list, nvars, ncubes)
+    ans = format(output)
+    print(ans)
+    if __name__ == "__main__":
+        main()
